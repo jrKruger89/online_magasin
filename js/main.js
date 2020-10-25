357a60bda089a25d28fa043893f40a0ae9be079b
jrKruger89 <kruger89@hotmail.com>
2020-10-25 19:06:08 +0100

    Pages added
    Added subfolders and pages

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(function () {
 
+
     $('.main_nav_toggle').click(function() {
         $('.main_nav').slideToggle();
         $('.sub_nav').css('display', 'none');
-    })
+    });
 
     $('.sub_nav').hide();
     $(".nav_item:has(ul)").click(function () {
         $(".main_nav").find('.sub_nav').slideUp();
         $(this).find('.sub_nav').slideDown();
     });
-
 
 });
